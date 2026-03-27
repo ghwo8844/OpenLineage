2431aab9cefbc44b46aed04c83507cc8cb5fe0d6
Hojae Jung <hojaej@netflix.com>
2026-03-27 10:55:06 -0700

    Handle CassandraTables

--- integration/spark/app/src/main/java/io/openlineage/spark/agent/lifecycle/DefaultRelationHandlers.java
+++ integration/spark/app/src/main/java/io/openlineage/spark/agent/lifecycle/DefaultRelationHandlers.java
@@ -8,6 +8,7 @@ package io.openlineage.spark.agent.lifecycle;
 import com.google.common.collect.ImmutableList;
 import io.openlineage.spark.agent.lifecycle.plan.catalog.RelationHandler;
 import io.openlineage.spark.api.OpenLineageContext;
+import io.openlineage.spark3.agent.lifecycle.plan.catalog.CassandraRelationHandler;
 import io.openlineage.spark3.agent.lifecycle.plan.catalog.iceberg.IcebergRelationHandler;
 import java.util.List;
 
@@ -16,6 +17,6 @@ final class DefaultRelationHandlers {
   private DefaultRelationHandlers() {}
 
   static List<RelationHandler> list(OpenLineageContext context) {
-    return ImmutableList.of(new IcebergRelationHandler(context));
+    return ImmutableList.of(new CassandraRelationHandler(), new IcebergRelationHandler(context));
   }
 }
